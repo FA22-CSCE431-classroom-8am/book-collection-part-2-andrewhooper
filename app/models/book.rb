3892e6c2dca142adeeb86df68215e75481066abd
root <root@localhost>
2022-09-16 17:21:23 +0000

    Created User and UserBook entities and established relations between the different entities. Also modified the index.html.erb and _form.html.erb under the user_books folder in view along with making the user_books/index the root

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
+	has_many :users, through: :user_books
+	has_many :user_books
+
+	has_many :users, through: :userbooknews
+	has_many :userbooknews
 	validates :title, :author, :price, :published_date, presence: true
 end
